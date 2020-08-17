884df85e533206c6114d25fbca169738b807d367
khoado24 <khoado24@gmail.com>
2020-08-17 21:25:51 +0700

    Voice

--- IHM/KDMUSIQUE/js/search.js
+++ IHM/KDMUSIQUE/js/search.js
@@ -1,5 +1,35 @@
 function SearchByVoice() {
     var popup = document.getElementById("popupContent");
+    var backgr = document.getElementById("ResultFound");
+    if (backgr.style.opacity == 0.2)
+        backgr.style.opacity = 1;
+    else
+        backgr.style.opacity = 0.2;
     popup.classList.toggle("show");
+}
+function startDictation() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+        var recognition = new webkitSpeechRecognition();
+
+        recognition.continuous = false;
+        recognition.interimResults = false;
+
+        recognition.lang = "en-US";
+        recognition.start();
+
+        recognition.onresult = function (e) {
+            document.getElementById('transcript').value
+                = e.results[0][0].transcript;
+            recognition.stop();
+            document.getElementById('labnol').submit();
+        };
+
+        recognition.onerror = function (e) {
+            recognition.stop();
+        }
+
+    }
 }
 
