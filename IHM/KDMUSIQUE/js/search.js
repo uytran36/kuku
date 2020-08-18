d98277b3d2fb44972529e9309b7ed16cd2d44059
khoado24 <khoado24@gmail.com>
2020-08-18 22:59:15 +0700

    Voice

--- IHM/KDMUSIQUE/js/search.js
+++ IHM/KDMUSIQUE/js/search.js
@@ -1,10 +1,22 @@
 function SearchByVoice() {
-    var popup = document.getElementById("popupContent");
-    var backgr = document.getElementById("ResultFound");
-    if (backgr.style.opacity == 0.2)
-        backgr.style.opacity = 1;
+    startDictation();
+    var popup = document.getElementById("popupContent");;
+    var temp = document.getElementById("temp");
+    if (temp.style.visibility == 'visible')
+    {    
+        temp.style.visibility = 'hidden';
+    }
     else
-        backgr.style.opacity = 0.2;
+    {
+        temp.style.visibility = 'visible'
+        temp.style.position = 'fixed';
+        temp.style.background = 'black';
+        temp.style.opacity = 0.6;
+        temp.style.width = '100%';
+        temp.style.height = '100%';
+        temp.style.top = 0;
+        temp.style.left = 0;
+    }
     popup.classList.toggle("show");
 }
 function startDictation() {
@@ -24,6 +36,8 @@ function startDictation() {
                 = e.results[0][0].transcript;
             recognition.stop();
             document.getElementById('labnol').submit();
+            document.getElementById('transcript').value
+                = e.results[0][0].transcript;
         };
 
         recognition.onerror = function (e) {
